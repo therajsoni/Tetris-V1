2f2e559dd1cff59c6164c888d712e2c0ad7f52e0
therajsoni <rajsoni909192@gmail.com>
2026-02-13 17:53:03 +0530

    1

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ratmdatastore" # Replace with your actual S3 bucket name
+    bucket = "rajsoni-8103615769" # Replace with your actual S3 bucket name
     key    = "eks/terraform.tfstate"
     region = "ap-south-1"
   }
